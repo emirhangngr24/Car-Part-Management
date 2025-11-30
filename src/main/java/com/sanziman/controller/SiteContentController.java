269896fd5e4a9048c246b6ae763ed53f98205940
emirhangngr24 <emirhangungor2424@hotmail.com>
2025-11-30 18:44:27 +0300

    Update SiteContentController.java

--- src/main/java/com/sanziman/controller/SiteContentController.java
+++ src/main/java/com/sanziman/controller/SiteContentController.java
@@ -13,7 +13,6 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "*")
 public class SiteContentController {
 
     private final SiteContentService service;
